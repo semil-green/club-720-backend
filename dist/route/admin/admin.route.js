5744e0991bd59247bbd7251e5a41c9e5dac8d726
semil21 <m7semil@gmail.com>
2025-12-19 10:52:58 +0530

    removed dist

--- dist/route/admin/admin.route.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { createNewUserController, userLoginController } from "../../controller/admin/admin.controller";
-const adminRouter = express.Router();
-adminRouter.post("/create", createNewUserController);
-adminRouter.post("/login", userLoginController);
-export default adminRouter;
