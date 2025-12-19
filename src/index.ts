e6f9ce22b6b0457c26fe557e010c82ac7d6dd869
semil21 <m7semil@gmail.com>
2025-12-19 13:26:56 +0530

    checking server running

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,65 @@
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+
+// process.on("uncaughtException", (err) => {
+//     console.error("UNCAUGHT EXCEPTION:", err);
+// });
+
+// process.on("unhandledRejection", (reason) => {
+//     console.error("UNHANDLED REJECTION:", reason);
+// });
+
+// import connectDatabase from "./database/database";
+// import blogRouter from "./route/blog/blog.route";
+// import categoryRouter from "./route/category/category.route";
+// import teamMemberRouter from "./route/team-member/teamMember.route";
+// import authorRouter from "./route/author/author.route";
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.get("/", (_req, res) => {
+//     res.status(200).send("Backend is alive");
+// });
+
+// app.get("/health", (_req, res) => {
+//     res.status(200).send("OK");
+// });
+
+// app.use("/user", teamMemberRouter);
+// app.use("/blog", blogRouter);
+// app.use("/category", categoryRouter);
+// app.use("/author", authorRouter);
+
+// console.log("BOOT: index.ts loaded");
+
+// const startServer = async () => {
+//     await connectDatabase();
+
+//     const PORT = process.env.PORT || 8080;
+
+//     app.listen(PORT, () => {
+//         console.log(`Server running on port ${PORT}`);
+//     });
+// };
+
+// startServer();
+
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-
-process.on("uncaughtException", (err) => {
-    console.error("UNCAUGHT EXCEPTION:", err);
-});
-
-process.on("unhandledRejection", (reason) => {
-    console.error("UNHANDLED REJECTION:", reason);
-});
-
-import connectDatabase from "./database/database";
-import blogRouter from "./route/blog/blog.route";
-import categoryRouter from "./route/category/category.route";
-import teamMemberRouter from "./route/team-member/teamMember.route";
-import authorRouter from "./route/author/author.route";
-
-dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (_req, res) => {
-    res.status(200).send("Backend is alive");
-});
-
 app.get("/health", (_req, res) => {
-    res.status(200).send("OK");
+    res.send("OK");
 });
 
-app.use("/user", teamMemberRouter);
-app.use("/blog", blogRouter);
-app.use("/category", categoryRouter);
-app.use("/author", authorRouter);
+const PORT = process.env.PORT || 8080;
 
-console.log("BOOT: index.ts loaded");
-
-const startServer = async () => {
-    await connectDatabase();
-
-    const PORT = process.env.PORT || 8080;
-
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-};
-
-startServer();
+app.listen(PORT, () => {
+    console.log("Server running on port", PORT);
+});
